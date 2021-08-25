a0d9c5cf45fada5de728b909919bc689398b6a50
Naman <07032001nk@gmail.com>
2021-08-25 20:01:00 +0530

    added

---
+++ app/config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+        $site_name = "DEMO";
+
+        $site_url = "http://localhost";
+
+        
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require 'app/core.php';head();?>
+
+        <div class="container mt-5">
+                <h1>hello world</h1>
+                <p>
+                        <a href="/register">register</a>
+                        <br>
+                        <a href="/login">login</a>
+                </p>
+        </div>
+
+<?=foot();?>
